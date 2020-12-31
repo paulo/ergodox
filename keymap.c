d46a5752cc04b0925549b30cb5142c290ea9cbd8
Paulo <paulorccaraujo@gmail.com>
2020-12-31 11:05:35 +0100

    Update linux mappings so Ctrl key is the same on Mac and Linux

--- keymap.c
+++ keymap.c
@@ -16,6 +16,12 @@
 #include "keymap_danish.h"
 #include "keymap_norwegian.h"
 #include "keymap_portuguese.h"
+#include "keymap_contributions.h"
+#include "keymap_czech.h"
+#include "keymap_romanian.h"
+#include "keymap_russian.h"
+#include "keymap_uk.h"
+#include "keymap_estonian.h"
 
 #define KC_MAC_UNDO LGUI(KC_Z)
 #define KC_MAC_CUT LGUI(KC_X)
@@ -51,10 +57,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT_ergodox_pretty(
     KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_6,                                           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       KC_EQUAL,       KC_BSPACE,
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           MO(3),                                          KC_SLASH,       KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
-    KC_LGUI,        KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
+    KC_LCTRL,       KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
     KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           MO(2),                                          KC_COLN,        KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSHIFT,
     KC_LPRN,        KC_RPRN,        KC_GRAVE,       KC_LALT,        KC_LCTRL,                                                                                                       KC_RCTRL,       KC_RALT,        KC_LBRACKET,    KC_RBRACKET,    MO(4),
-                                                                                                    KC_HYPR,        KC_TRANSPARENT, KC_MEH,         KC_HYPR,
+                                                                                                    KC_LGUI,        KC_TRANSPARENT, KC_MEH,         KC_HYPR,
                                                                                                                     KC_HOME,        KC_PGUP,
                                                                                     KC_BSPACE,      KC_DELETE,      KC_END,         KC_PGDOWN,      KC_ENTER,       KC_SPACE
   ),
--- rules.mk
+++ rules.mk
@@ -1,5 +1,6 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
-LINK_TIME_OPTIMIZATION_ENABLE = yes
+LTO_ENABLE = yes
+CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
 RGBLIGHT_ENABLE = no
